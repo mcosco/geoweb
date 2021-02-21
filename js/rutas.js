fbc9b951e7687496ec6abb4dfcc4c0cf41575251
mcosco <ri_marta@hotmail.com>
2021-02-21 11:36:11 +0100

    terremotos

--- js/rutas.js
+++ js/rutas.js
@@ -28,4 +28,19 @@ function zoomToRutas(valores) {
             zoom: coord[0]
         });
 
-    } //fin funcion
+} //fin funcion
+
+var animacion;
+function rotarCamara(timestamp) {
+
+    rotacion =timestamp /100 ==360 ?0 : timestamp /100;
+    map.rotateTo(rotacion, { duration: 0 });
+
+    animacion = requestAnimationFrame(rotarCamara);
+}
+
+
+function finalRotarCamara() {
+
+    cancelAnimationFrame(animacion);
+}   //fin funcion
